84d8eb436679fb8433247af604b0b4c8846a5d9b
RockLegend93 <milan.maric.ja@gmail.com>
2016-06-04 19:21:50 +0200

    CRUD

--- app/src/main/java/net/etfbl/prs/prs112212_z4/MainActivity.java
+++ app/src/main/java/net/etfbl/prs/prs112212_z4/MainActivity.java
@@ -1,7 +1,11 @@
 package net.etfbl.prs.prs112212_z4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 /****************************************************************************
@@ -25,8 +29,14 @@ import android.widget.ListView;
  * <p/>
  **********************************************************************/
 public class MainActivity extends AppCompatActivity {
+    public static final String ACTION_NEW_RECIPE = "ACTION_NEW_RECIPE";
+    public static final int REQUEST_CODE = 1;
+    public static final String ACTION_UPDATE_RECIPE = "ACTION_UPDATE_RECIPE";
+    public static final String EXTRA_RECIPE = "EXTRA_RECIPE";
+    private static final int REQUEST_CODE_UPDATE = 2;
     private RecipeAdapter adapter;
     private RecipeDbHelper helper;
+    private FloatingActionButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +44,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         adapter = new RecipeAdapter(this);
         ListView listView = (ListView) findViewById(R.id.list);
+        if (listView != null) {
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    RecipeDTO recipeDTO = (RecipeDTO) adapter.getItem(position);
+                    Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+                    intent.setAction(ACTION_UPDATE_RECIPE);
+                    intent.putExtra(EXTRA_RECIPE, recipeDTO);
+                    startActivityForResult(intent, REQUEST_CODE_UPDATE);
+
+                }
+            });
+        }
+        button = (FloatingActionButton) findViewById(R.id.newRecipe);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+                intent.setAction(ACTION_NEW_RECIPE);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
         listView.setAdapter(adapter);
         helper = new RecipeDbHelper(this);
         updateList();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        updateList();
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+
+        }
     }
 
     private void updateList() {
--- app/src/main/java/net/etfbl/prs/prs112212_z4/RecipeActivity.java
+++ app/src/main/java/net/etfbl/prs/prs112212_z4/RecipeActivity.java
@@ -1,13 +1,101 @@
 package net.etfbl.prs.prs112212_z4;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RecipeActivity extends AppCompatActivity {
+
+    private String action;
+    private RecipeDbHelper helper;
+    private RecipeDTO recipe;
+    private EditText durationEditText;
+    private EditText nameEditText;
+    private EditText ingredientsEditText;
+    private EditText prepartationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        getEditTextRefs();
+        Intent intent = getIntent();
+        action = intent.getAction();
+        if (MainActivity.ACTION_NEW_RECIPE.equals(action)) {
+            recipe = new RecipeDTO();
+        } else {
+            recipe = (RecipeDTO) intent.getSerializableExtra(MainActivity.EXTRA_RECIPE);
+            recipe2View();
+        }
+
+        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.newRecipe);
+        helper = new RecipeDbHelper(this);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch (action) {
+                        case MainActivity.ACTION_NEW_RECIPE:
+                            if (view2Recipe()) {
+                                helper.insert(recipe);
+                                Intent result = new Intent();
+                                setResult(Activity.RESULT_OK, result);
+                                finish();
+                            }
+                            break;
+                        case MainActivity.ACTION_UPDATE_RECIPE:
+                            if (view2Recipe()) {
+                                helper.update(recipe);
+                                Intent result = new Intent();
+                                setResult(Activity.RESULT_OK, result);
+                                finish();
+                            }
+                        default:
+                            Toast.makeText(RecipeActivity.this, R.string.error, Toast.LENGTH_SHORT).show();
+                            break;
+
+                    }
+                }
+            });
+        }
+
     }
+
+    private void getEditTextRefs() {
+        durationEditText = (EditText) findViewById(R.id.r_duration);
+        nameEditText = (EditText) findViewById(R.id.r_name);
+        ingredientsEditText = (EditText) findViewById(R.id.r_ingredients);
+        prepartationEditText = (EditText) findViewById(R.id.r_prepare);
+    }
+
+    private void recipe2View() {
+        durationEditText.setText(recipe.getDuration());
+        nameEditText.setText(recipe.getName());
+        ingredientsEditText.setText(recipe.getIngredients());
+        prepartationEditText.setText(recipe.getPrepare());
+    }
+
+    private boolean view2Recipe() {
+        //TODO add validation
+        recipe.setDuration(Integer.parseInt(durationEditText.getText().toString()));
+        recipe.setName(nameEditText.getText().toString());
+        recipe.setIngredients(ingredientsEditText.getText().toString());
+        recipe.setPrepare(prepartationEditText.getText().toString());
+        return true;
+    }
+
+    private boolean checkText(String text) {
+        if (text == null)
+            return false;
+        if ("".equals(text))
+            return false;
+        return true;
+    }
+
+
 }
